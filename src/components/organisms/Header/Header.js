df700399442c8252d8dba4c5ebd7929f8e76539e
Scott Moon <jiwonsis@naver.com>
2018-01-11 17:49:07 +0900

    ScreenMask 생성

---
+++ src/components/organisms/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Header.scss';
+import classNames from 'classnames/bind';
+import { Logo, HeaderNav, Button } from 'components';
+
+const cx = classNames.bind(styles);
+
+const Header = () => {
+  return (
+    <div className={cx('header')}>
+      <div className={cx('responsive')}>
+        <div className={cx('logo-wrapper')}>
+          <Logo/>
+        </div>
+        <div className={cx('right-side')}>
+          <HeaderNav/>
+          <Button invert className={cx('login-button')}>로그인</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
