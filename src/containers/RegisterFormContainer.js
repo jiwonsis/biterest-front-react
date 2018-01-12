90b0cd3837694470c6801bd2e437ff2f958239ed
Scott Moon <jiwonsis@naver.com>
2018-01-13 06:09:28 +0900

    가입 완료 (부분적)
    - 이메일, 닉네임, 비밀번호로 가입은 되나, 초기 지급 수단으로는 가입이 되지 않는다. 그 이유는 API를 아직 안만들어서 이다.
    - 리엑트 핫로더 부분을 패키지에서 제거 했더니 문제가 생겼다. 그래서 다시 설정을 하게되었다.
    - 프록시 서버를 4000번으로 해놨는데, 브라우저에서는 3000번 포트로 나가서 이상해서 다시 봤는데, 잘된다. 그냥 이렇게 쓰자.

--- src/containers/RegisterFormContainer.js
+++ src/containers/RegisterFormContainer.js
@@ -4,6 +4,8 @@ import { RegisterForm } from 'components';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as registerActions from 'store/modules/register';
+import debounce from 'lodash/debounce'
+import { withRouter } from 'react-router';
 
 class RegisterFormContainer extends Component {
   handleChangeNickName = (e) => {
@@ -11,6 +13,17 @@ class RegisterFormContainer extends Component {
     const { RegisterActions } = this.props;
 
     RegisterActions.changeNickName(value);
+    this.checkDisplayName(value);
+  }
+
+  checkDisplayName = debounce((value) => {
+    const { RegisterActions } = this.props;
+    RegisterActions.checkDisplayName(value);
+  }, 500);
+
+  handleNickNameBlur = () => {
+    const { nickName, RegisterActions } = this.props;
+    RegisterActions.checkDisplayName(nickName);
   }
 
   handleSetCurrency = (currency) => {
@@ -24,40 +37,55 @@ class RegisterFormContainer extends Component {
   }
 
   handleSubmit = () => {
-    const { nickName, currency, optionIndex } = this.props;
-    console.log(nickName, currency, optionIndex);
+    const { nickName, currency, optionIndex, authForm, RegisterActions } = this.props;
+    const { email, password } = authForm.toJS();
+
+    RegisterActions.submit({
+      displayName: nickName,
+      email,
+      password,
+      initialMoney: {
+        currency,
+        index: optionIndex
+      }
+    });
   }
 
 
   render() {
 
-    const { nickName, currency, optionIndex } = this.props;
+    const { nickName, currency, optionIndex, displayNameExists } = this.props;
     const {
       handleChangeNickName,
       handleSetCurrency,
       handleSelectOptionIndex,
-      handleSubmit
+      handleSubmit,
+      handleNickNameBlur
     } = this;
     return (
       <RegisterForm
         nickName={nickName}
         currency={currency}
         optionIndex={optionIndex}
+        displayNameExists={displayNameExists}
         onChangeNickname={handleChangeNickName}
         onSetCurrency={handleSetCurrency}
         onSelectOptionIndex={handleSelectOptionIndex}
-        onSubmit={handleSubmit} />
+        onSubmit={handleSubmit} 
+        onNicknameBlur={handleNickNameBlur} />
     );
   }
 }
 
 export default connect(
   (state) => ({
+    authForm: state.auth.get('form'),
     nickName: state.register.get('nickname'),
     currency: state.register.get('currency'),
-    optionIndex: state.register.get('optionIndex')
+    optionIndex: state.register.get('optionIndex'),
+    displayNameExists: state.register.get('displayNameExists')
   }),
   (dispatch) => ({
     RegisterActions: bindActionCreators(registerActions, dispatch)
   })
-)(RegisterFormContainer);
+)(withRouter(RegisterFormContainer));
--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,7 +1,31 @@
 import axios from 'axios';
 
 export const checkEmail = (email) => {
-  return axios.get(`/api/v1.0/auth/check-email/${email}`);
+  return axios.get(`/api/v1.0/auth/exists/email/${email}`);
 }
 
-// export const checkEmail = (email) => axios.get('/api/v1.0/auth/check-email/' + email);
+export const checkDisplayName = (displayName) => {
+  let api = `/api/v1.0/auth/exists/display-name/${displayName}`;
+  console.log(api);
+  return axios.get(api);
+}
+
+export const localRegister = ({
+  displayName,
+  email,
+  password,
+  initialMoney: { currency, index }
+}) => {
+  console.log({displayName,
+    email,
+    password,
+    initialMoney: { currency, index }});
+  return (
+    axios.post('/api/v1.0/auth/register/local', {
+      displayName,
+      email,
+      password,
+      // initialMoney: { currency, index }
+    })
+  )
+}
