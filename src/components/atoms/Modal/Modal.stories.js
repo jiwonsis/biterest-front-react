82dbb27d17e22afaa98c040db672d8113f4a49f9
Scott Moon <jiwonsis@naver.com>
2018-01-12 16:18:35 +0900

    1. Reducer   - 이슈 사항: Reducer 키를 찾지 못하는 현상   - 원인 : 정규식 오류   - 현재 : 해결 2. atoms.Input scss 스타일 미적용   - 이슈 사항 : 스타일 미적용   - 원인 : 임포트 명령어 누락   - 현재 : 해결

--- src/components/atoms/Modal/Modal.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Modal from './Modal';
-
-storiesOf('Modal', module).add('default', () => {
-  return (
-    <Modal>으악</Modal>
-  )
-});
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux';
 
 // imports all file except index.js
-const req = require.context('./', true, /^(?!\/index).*.js$/);
+const req = require.context('./', true, /^(?!.\/index).*.js$/);
 
 const modules = {};
 
 req.keys().forEach((key) => {
-  console.log(key);
-  let regex = /.\/(.*?).js$/;
+
+  const regex =  /.\/(.*?).js$/;
   const moduleName = regex.test(key) && key.match(regex)[1];
   modules[moduleName] = req(key).default;
 });
