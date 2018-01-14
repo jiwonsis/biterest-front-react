4bad08bad20b4904ecd41342a46304a809590a50
Scott Moon <jiwonsis@naver.com>
2018-01-15 02:17:40 +0900

    - 로셜 로그인 리팩토링 및 로그아웃 함수 추가

--- src/lib/social.js
+++ src/lib/social.js
@@ -9,24 +9,46 @@ hello.init({
 
 export default(function() {
   return {
-    facebook: () => {
+    // facebook: () => {
+    //   return new Promise((resolve, reject) => {
+    //     // hellojs 는 일반 Promise 가 아닌 Promise A+ 를 사용하므로, Promise 로 감싸줌
+    //     hello.login('facebook', {
+    //       scope: 'email'
+    //     }).then(
+    //       auth => resolve(auth.authResponse.access_token),
+    //       e => reject(e)
+    //     )
+    //   })
+    // },
+    // google: () => {
+    //   return new Promise((resolve, reject) => {
+    //     hello.login('google', {
+    //       scope: 'email'
+    //     }).then(
+    //       auth => resolve(auth.authResponse.access_token),
+    //       e => reject(e)
+    //     )
+    //   })
+    // },
+    login: (type, scope) => {
       return new Promise((resolve, reject) => {
         // hellojs 는 일반 Promise 가 아닌 Promise A+ 를 사용하므로, Promise 로 감싸줌
-        hello.login('facebook', {
-          scope: 'email'
-        }).then(
-          auth => resolve(auth.authResponse.access_token),
-          e => reject(e)
+        hello
+          .login(type, {
+            scope: scope
+          }).then(
+            auth => resolve(auth.authResponse.access_token),
+            e => reject(e)
         )
       })
     },
-    google: () => {
+    logout: (type, scope) => {
       return new Promise((resolve, reject) => {
-        hello.login('google', {
-          scope: 'email'
-        }).then(
-          auth => resolve(auth.authResponse.access_token),
-          e => reject(e)
+        hello
+          .logout(type)
+          .then(
+            succeed => resolve('succeed'),
+            e => reject(e)
         )
       })
     }
